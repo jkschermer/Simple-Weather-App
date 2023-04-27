c477b65e72f09aeb1ba861281eaf7466dbf33295
jason <jasonschermer11@gmail.com>
2023-04-28 00:14:18 +0200

    added a method for selecting the most common weather icon of the weather predictions

--- domain/src/main/java/nl/simpleapp/domain/FetchWeatherForecast.kt
+++ domain/src/main/java/nl/simpleapp/domain/FetchWeatherForecast.kt
@@ -20,33 +20,54 @@ class FetchWeatherForecast(private val weatherForecastRepository: WeatherForecas
             val subList = forecastList.subList(startIndex, endIndex)
             val maxTemp = subList.maxOfOrNull { it.main.temp_max } ?: 0.0
             val minTemp = subList.minOfOrNull { it.main.temp_min } ?: 0.0
+            val frequencyMap: HashMap<String, Int> = HashMap()
+            val icons = subList.flatMap { forecastData -> forecastData.weather.map { it.icon } }
+            val icon = selectMostCommonWeatherIcon(icons, frequencyMap)
 
-            val randomIcon = subList.random().weather.random().icon
-            subList.map { it ->
-                weatherForecastData = WeatherForecastData(
-                    date = it.date,
-                    main = Main(
-                        temp = it.main.temp,
-                        feels_like = it.main.feels_like,
-                        temp_min = minTemp,
-                        temp_max = maxTemp,
-                        pressure = it.main.pressure,
-                        humidity = it.main.humidity,
-                    ),
-                    weather = it.weather.map {
-                        Weather(
-                            id = it.id,
-                            main = it.main,
-                            description = it.description,
-                            icon = randomIcon
-                        )
-                    },
-                    wind = it.wind
-                )
+            icon?.let { ic ->
+                subList.map { it ->
+                    weatherForecastData = WeatherForecastData(
+                        date = it.date, main = Main(
+                            temp = it.main.temp,
+                            feels_like = it.main.feels_like,
+                            temp_min = minTemp,
+                            temp_max = maxTemp,
+                            pressure = it.main.pressure,
+                            humidity = it.main.humidity,
+                        ), weather = it.weather.map {
+                            Weather(
+                                id = it.id,
+                                main = it.main,
+                                description = it.description,
+                                icon = ic
+                            )
+                        }, wind = it.wind
+                    )
+                }
             }
             weatherForecastData?.let { result.add(it) }
             startIndex = endIndex
         }
         return result
     }
+}
+
+private fun selectMostCommonWeatherIcon(icons: List<String>, frequencyMap: HashMap<String, Int>): String? {
+    var icon: String? = null
+    var max = 0
+
+    for (value in icons) {
+        frequencyMap[value] = frequencyMap.getOrDefault(value, 0) + 1
+    }
+
+    for (value in frequencyMap.keys) {
+        val frequency = frequencyMap[value]
+        if (frequency != null) {
+            if (frequency > max) {
+                max = frequency
+                icon = value
+            }
+        }
+    }
+    return icon
 }
